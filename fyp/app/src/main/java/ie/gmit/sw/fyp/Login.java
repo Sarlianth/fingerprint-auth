8a7e136ee4090b06dcff1e5c41bf1a66fd80b07a
Adrian Sypos <adrian00935@gmail.com>
2018-04-09 19:47:00 +0100

    Mapping objects

--- fyp/app/src/main/java/ie/gmit/sw/fyp/Login.java
+++ fyp/app/src/main/java/ie/gmit/sw/fyp/Login.java
@@ -11,6 +11,4 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
 
     }
-
-
 }
--- fyp/app/src/main/java/ie/gmit/sw/fyp/RestAPI.java
+++ fyp/app/src/main/java/ie/gmit/sw/fyp/RestAPI.java
@@ -2,13 +2,24 @@ package ie.gmit.sw.fyp;
 
 import android.support.v7.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class RestAPI extends AppCompatActivity {
 
@@ -47,6 +58,48 @@ public class RestAPI extends AppCompatActivity {
         InputStream istream = conn.getInputStream();
         String result = convertStreamToString(istream);
         return result;
+    }
+
+    // Reference https://stackoverflow.com/questions/2989560/how-to-get-the-fields-in-an-object-via-reflection
+    private Object mapObject(Object o) {
+        Object finalValue = null;
+        if (o.getClass() == String.class) {
+            finalValue = o;
+        }
+        else if (Number.class.isInstance(o)) {
+            finalValue = String.valueOf(o);
+        } else if (Date.class.isInstance(o)) {
+            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss", new Locale("en", "USA"));
+            finalValue = sdf.format((Date)o);
+        }
+        else if (Collection.class.isInstance(o)) {
+            Collection<?> col = (Collection<?>) o;
+            JSONArray jArray = new JSONArray();
+            for (Object item : col) {
+                jArray.put(mapObject(item));
+            }
+            finalValue = jArray;
+        } else {
+            Map<String, Object> map = new HashMap<>();
+            Method[] methods = o.getClass().getMethods();
+            for (Method method : methods) {
+                if (method.getDeclaringClass() == o.getClass()
+                        && method.getModifiers() == Modifier.PUBLIC
+                        && method.getName().startsWith("get")) {
+                    String key = method.getName().substring(3);
+                    try {
+                        Object obj = method.invoke(o, null);
+                        Object value = mapObject(obj);
+                        map.put(key, value);
+                        finalValue = new JSONObject(map);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+        return finalValue;
     }
 }
 
