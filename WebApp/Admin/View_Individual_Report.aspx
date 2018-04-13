6a508fe22c42687f855cccdc5be76797a7006023
Adrian Sypos <adrian00935@gmail.com>
2018-04-13 14:06:04 +0100

    got individual report done with data visualization displaying data in a chart

---
+++ WebApp/Admin/View_Individual_Report.aspx
@@ -0,0 +1,121 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/MasterPage.master" CodeFile="View_Individual_Report.aspx.cs" Inherits="View_Individual_Report" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
+<asp:Content ID="body" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table width="80%">
+        <tr>
+            <td colspan="2">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center" class="hea">
+                <asp:Label ID="Label1" runat="server" Text="Individual report"></asp:Label></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="lab" width="40%">
+                <asp:Label ID="Label2" runat="server" Text="Class"></asp:Label></td>
+            <td class="tx">
+                <asp:DropDownList ID="DropDownList1" runat="server" Width="40%"
+                    AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                    <asp:ListItem>--Select--</asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="lab">
+                <asp:Label ID="Label3" runat="server" Text="Student name"></asp:Label></td>
+            <td class="tx">
+                <asp:DropDownList ID="DropDownList2" runat="server" Width="40%">
+                    <asp:ListItem>--Select--</asp:ListItem>
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="lab">
+                <asp:Label ID="Label4" runat="server" Text="From date"></asp:Label></td>
+            <td class="tx">
+                <asp:TextBox ID="TextBox1" type="date" required="true" runat="server" Width="50%"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="lab">
+                <asp:Label ID="Label5" runat="server" Text="To date"></asp:Label></td>
+            <td class="tx">
+                <asp:TextBox ID="TextBox2" type="date" required="true" runat="server" Width="50%"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <asp:Button ID="Button1" runat="server" Text="Submit" Height="50px" OnClick="Button1_Click" CssClass="intabular" Width="20%" /></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <br />
+            </td>
+        </tr>
+    </table>
+    <asp:Panel ID="Panel1" runat="server">
+
+        <table width="80%">
+            <tr>
+                <td colspan="2" align="right">
+                    <asp:Image ID="Image1" runat="server" ImageUrl="~/images/legend.png" /></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center">
+                    <asp:Chart ID="Chart2" runat="server" Width="1000px" Palette="EarthTones">
+                        <Series>
+                            <asp:Series ChartArea="ChartArea1" ChartType="Line" Name="Series1">
+                            </asp:Series>
+                        </Series>
+                        <ChartAreas>
+                            <asp:ChartArea Name="ChartArea1">
+                                <AxisX IsStartedFromZero="True" Title="Date"
+                                    TitleFont="Segoe UI, 10pt, style=Bold" IsLabelAutoFit="False" LineColor="Gray"
+                                    Minimum="0" Maximum="20" Interval="1">
+                                </AxisX>
+                                <AxisY IsStartedFromZero="True" Title="Status"
+                                    TitleFont="Segoe UI, 10pt, style=Bold" IsLabelAutoFit="False" LineColor="Gray"
+                                    Minimum="0" Maximum="4">
+                                </AxisY>
+                            </asp:ChartArea>
+                        </ChartAreas>
+
+                    </asp:Chart>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br />
+                </td>
+            </tr>
+        </table>
+    </asp:Panel>
+</asp:Content>
---
+++ WebApp/Admin/View_Individual_Report.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class View_Individual_Report : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=182.50.133.110;Initial Catalog=AAttendance;User ID=AAttendance;Password=H0vru@85");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Panel1.Visible = false;
+            SqlDataAdapter adapter;
+            DataSet dataSet = new DataSet();
+            string query = "select course_name from add_class";
+            adapter = new SqlDataAdapter(query, con);
+            adapter.Fill(dataSet);
+            if (dataSet.Tables[0].Rows.Count > 0)
+            {
+                for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
+                {
+                    DropDownList1.Items.Add(dataSet.Tables[0].Rows[i][0].ToString());
+                }
+            }
+        }
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (DropDownList2.SelectedItem.Text == "--Select--")
+        {
+            Page.ClientScript.RegisterStartupScript(GetType(), "msgtype()", "alert('Please select student')", true);
+        }
+        else
+        {
+            string fromDate = TextBox1.Text;
+            fromDate = convert(fromDate);
+            DateTime from = Convert.ToDateTime(fromDate);
+            int fromMonth = from.Month;
+
+            string toDate = TextBox2.Text;
+            toDate = convert(toDate);
+            DateTime to = Convert.ToDateTime(toDate);
+            int toMonth = to.Month;
+            int month_now = DateTime.Now.Month;
+            SqlDataAdapter adapter;
+            DataSet dataSet = new DataSet();
+            string query = "select sid from student_details where class='" + DropDownList1.SelectedItem.Text + "' and name='" + DropDownList2.SelectedItem.Text + "'";
+            adapter = new SqlDataAdapter(query, con);
+            adapter.Fill(dataSet);
+
+            if (dataSet.Tables[0].Rows.Count > 0)
+            {
+                SqlDataAdapter adapter1;
+                DataSet dataSet1 = new DataSet();
+                string query1 = "select att_date,status from attendance where sid='" + dataSet.Tables[0].Rows[0][0].ToString() + "' and att_date>='" + fromDate + "' and att_date<='" + toDate + "'";
+                adapter1 = new SqlDataAdapter(query1, con);
+                adapter1.Fill(dataSet1);
+                DataTable ChartData = dataSet1.Tables[0];
+
+                // storing total rows count to loop on each record  
+                string[] XPointMember = new string[dataSet1.Tables[0].Rows.Count];
+                int[] YPointMember = new int[dataSet1.Tables[0].Rows.Count];
+
+                for (int count = 0; count < ChartData.Rows.Count; count++)
+                {
+                    // values for X axis  
+                    XPointMember[count] = dataSet1.Tables[0].Rows[count][0].ToString();
+                    // vlues for Y Axis  
+                    string status = dataSet1.Tables[0].Rows[count][1].ToString();
+                    if (status == "Present")
+                    {
+                        YPointMember[count] = 4;
+                    }
+                    else if (status == "Absent")
+                    {
+                        YPointMember[count] = 1;
+
+                    }
+                    else if (status == "Late")
+                    {
+                        YPointMember[count] = 3;
+
+                    }
+                    else if (status == "Sick")
+                    {
+                        YPointMember[count] = 2;
+
+                    }
+                }
+                // binding chart control  
+                Chart2.Series[0].Points.DataBindXY(XPointMember, YPointMember);
+                // setting width of line  
+                Chart2.Series[0].BorderWidth = 1;
+                // setting Chart type  
+                Chart2.ChartAreas["ChartArea1"].AxisX.MajorGrid.Enabled = false;
+                Chart2.ChartAreas["ChartArea1"].AxisY.MajorGrid.Enabled = false;
+                Panel1.Visible = true;
+            }
+        }
+    }
+
+    public string convert(string str)
+    {
+        DateTime dateTime = Convert.ToDateTime(str);
+        string converted = dateTime.ToString("yyyy/MM/dd");
+        return converted;
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (DropDownList1.SelectedItem.Text == "--Select--")
+        {
+            Page.ClientScript.RegisterStartupScript(GetType(), "msgtype", "alert('Select Student Name !!!')", true);
+        }
+        else
+        {
+            SqlDataAdapter adapter;
+            DataSet dataSet = new DataSet();
+            string query = "select name from student_details where class='" + DropDownList1.SelectedItem.Text + "'";
+            adapter = new SqlDataAdapter(query, con);
+            adapter.Fill(dataSet);
+            DropDownList2.Items.Clear();
+            DropDownList2.Items.Add("--Select--");
+            if (dataSet.Tables[0].Rows.Count > 0)
+            {
+                for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
+                {
+                    DropDownList2.Items.Add(dataSet.Tables[0].Rows[i][0].ToString());
+                }
+            }
+        }
+    }
+}
